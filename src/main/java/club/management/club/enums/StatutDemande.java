db0cf45bf0e777f1bb2649386d7b932f6af74e44
bachriotmane <bachriotmane@gmail.com>
2024-11-10 20:01:54 +0100

    mv enums to enums package

---
+++ src/main/java/club/management/club/enums/StatutDemande.java
@@ -0,0 +1,5 @@
+package club.management.club.enums;
+
+public  enum StatutDemande {
+    EN_COURS, ACCEPTE, REFUSE
+}
---
+++ src/main/java/club/management/club/enums/TypeDemande.java
@@ -0,0 +1,5 @@
+package club.management.club.enums;
+
+public  enum TypeDemande {
+    CREATION_CLUB, INTEGRATION_CLUB, EVENEMENT
+}
