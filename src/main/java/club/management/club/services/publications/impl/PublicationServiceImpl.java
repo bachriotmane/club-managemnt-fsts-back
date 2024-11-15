1480c8013d06e0ceb69164f4f35b89feb8dc78fe
bachriotmane <bachriotmane@gmail.com>
2024-11-15 14:39:55 +0100

    complete fetch with specific date

--- src/main/java/club/management/club/services/publications/impl/PublicationServiceImpl.java
+++ src/main/java/club/management/club/services/publications/impl/PublicationServiceImpl.java
@@ -14,6 +14,8 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+
 @Service
 @AllArgsConstructor
 public class PublicationServiceImpl implements PublicationsService {
@@ -24,6 +26,16 @@ public class PublicationServiceImpl implements PublicationsService {
     @Override
     public Page<PublicationDTO> getAllPublications(int page, int size, boolean isDesc, boolean isPublic, String keyword, String formDate, String toDate) {
         Specification<Publication> publicationSpecifications = PublicationSpecifications.withTitle(keyword);
+
+        LocalDateTime parsedFromDate = formDate == null || formDate.isEmpty() ?  null : LocalDateTime.parse(formDate);
+        LocalDateTime parsedToDate = toDate == null || toDate.isEmpty() ?  null : LocalDateTime.parse(toDate);
+
+
+        if (parsedFromDate != null || parsedToDate != null) {
+            publicationSpecifications = Specification.where(publicationSpecifications)
+                    .and(PublicationSpecifications.withDateRange(parsedFromDate, parsedToDate));
+        }
+
         Pageable pageable =  PageRequest.of(page, size, Sort.by("date").descending());
         return publicationRepository.findAll(publicationSpecifications,pageable).map(publicationMapper::convertToDTO);
     }
