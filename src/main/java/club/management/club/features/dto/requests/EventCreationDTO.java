158e7bf96c45b698231a17f82f7f507cbb190840
Idrissi <idrissiabdelkarim01@gmail.com>
2024-11-20 21:09:31 +0100

    fixing DTO

--- src/main/java/club/management/club/features/dto/requests/EventCreationDTO.java
+++ src/main/java/club/management/club/features/dto/requests/EventCreationDTO.java
@@ -8,17 +8,14 @@ import java.time.LocalDateTime;
 
 public record EventCreationDTO(
         @NotEmpty(message = "Le nom d'evenment est  necessaire")
-        @NotNull(message = "Le nom d'evenment est  necessaire")
         String eventName,
         @NotEmpty(message = "une description est  necessaire")
-        @NotNull(message = "une description est  necessaire")
         String description,
         @NotEmpty(message = "Localisation est  necessaire")
-        @NotNull(message = "Localisation est  necessaire")
         String location,
 
         LocalDateTime eventDate,
         @NotNull(message = "un montant est  necessaire")
         Double budget
-) implements Serializable {
+){
 }
