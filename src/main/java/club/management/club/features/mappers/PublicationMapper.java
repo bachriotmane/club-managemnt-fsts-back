33b669557bcb198c3ed8932158b90e8c32277f7c
bachriotmane <bachriotmane@gmail.com>
2024-11-24 11:01:11 +0100

    complete add publication with image

--- src/main/java/club/management/club/features/mappers/PublicationMapper.java
+++ src/main/java/club/management/club/features/mappers/PublicationMapper.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class PublicationMapper {
     public PublicationDTO convertToDTO(Publication publication) {
-        return new PublicationDTO(publication.getId(), publication.getTitle(), publication.getPubDesc(),publication.getDate(), publication.isPublic(),publication.getClub().getNom(), publication.getClub().getIntegrations().size(),publication.getClub().getId() );
+        return new PublicationDTO(publication.getId(), publication.getTitle(), publication.getPubDesc(),publication.getDate(), publication.isPublic(),publication.getClub().getNom(), publication.getClub().getIntegrations().size(),publication.getClub().getId(),publication.getImage() == null ? null :  publication.getImage().getId() );
     }
 
     public Publication convertToPublication(PublicationDTO dto) {
