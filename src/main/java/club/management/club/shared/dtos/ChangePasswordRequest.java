037e5945a8206b81d242d070c7cf3c4505eb6f0d
Aramali Mohammed <mouhammedaramali@gmail.com>
2024-12-12 00:24:44 +0100

    add the cheks to change passwod

--- src/main/java/club/management/club/shared/dtos/ChangePasswordRequest.java
+++ src/main/java/club/management/club/shared/dtos/ChangePasswordRequest.java
@@ -2,6 +2,7 @@ package club.management.club.shared.dtos;
 
 public record ChangePasswordRequest(
         String oldPassword,
+        //add validate to check the new Password
         String newPassword,
         String email
 ) {
